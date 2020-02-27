5ef6a9f55b930a3aadf9e803d736f7596af59551
Tianhua Han <tiahua_han@yahoo.com>
2020-02-26 21:53:51 -0500

    new

---
+++ src/examples/button.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+
+#https://realpython.com/mobile-app-kivy-python/
+
+from kivy.app import App
+from kivy.uix.button import Button
+
+class ButtonApp(App):
+    def build(self):
+        return Button()
+
+    def on_press_button(self):
+        print('You pressed the button!')
+
+if __name__ == '__main__':
+    app = ButtonApp()
+    app.run()
